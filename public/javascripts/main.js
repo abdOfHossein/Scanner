a024978cddc78ffdb57a3ff9c96d30448fa7fa0a
abdOfHossein <92151678+abdOfHossein@users.noreply.github.com>
2022-11-02 12:47:39 +0330

    p1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,58 +4,41 @@
 
 /** Initiates a scan */
 
-let displayImagesOnPage=function (successful, mesg, response) {
-    if (!successful) {
-      // On error
-      console.error('Failed: ' + mesg);
-      alert(`mesg ===>${mesg}`)
-      alert(`response ===>${response}`)
-      return;
-    }
-  
-    if (
-      successful &&
-      mesg != null &&
-      mesg.toLowerCase().indexOf('user cancel') >= 0
-    ) {
-      // User cancelled.
-      console.info('User cancelled');
-      return;
-    }
-  
-    var scannedImages = scanner.getScannedImages(response, true, false); // returns an array of ScannedImage
-    for (
-      var i = 0;
-      scannedImages instanceof Array && i < scannedImages.length;
-      i++
-    ) {
-      var scannedImage = scannedImages[i];
-      processScannedImage(scannedImage);
-    }
+let displayImagesOnPage = function (successful, mesg, response) {
+  if (!successful) {
+    // On error
+    console.error('Failed: ' + mesg);
+    alert(`mesg ===>${mesg}`);
+    alert(`response ===>${response}`);
+    return;
   }
 
-$('#btn-scan').click(function (e) { 
-    e.preventDefault();
-    scanner.scan(displayImagesOnPage, {
-        output_settings: [
-          {
-            type: 'return-base64',
-            format: 'jpg',
-          },
-        ],
-      });
-    
-});
+  if (
+    successful &&
+    mesg != null &&
+    mesg.toLowerCase().indexOf('user cancel') >= 0
+  ) {
+    // User cancelled.
+    console.info('User cancelled');
+    return;
+  }
+  console.log('response', response);
+  var scannedImages = scanner.getScannedImages(response, true, false); // returns an array of ScannedImage
+  for (
+    var i = 0;
+    scannedImages instanceof Array && i < scannedImages.length;
+    i++
+  ) {
+    var scannedImage = scannedImages[i];
+    processScannedImage(scannedImage);
+  }
+};
 
-
-
-
-
-var imagesScanned = [];
-
+let imagesScanned = [];
 /** Processes a ScannedImage */
-function processScannedImage(scannedImage) {
+let processScannedImage = function (scannedImage) {
   imagesScanned.push(scannedImage);
+  console.log('scannedImage', scannedImage);
   var elementImg = scanner.createDomElementFromModel({
     name: 'img',
     attributes: {
@@ -64,4 +47,46 @@ function processScannedImage(scannedImage) {
     },
   });
   document.getElementById('images').appendChild(elementImg);
+  // $('#file').
+};
+
+//doing scan
+$('#btn-scan').click(function (e) {
+  e.preventDefault();
+  scanner.scan(displayImagesOnPage, {
+    output_settings: [
+      {
+        type: 'return-base64',
+        format: 'jpg',
+      },
+    ],
+  });
+});
+
+//==================================
+$('#btn-save-db').click(function (e) {
+  e.preventDefault();
+
+  submitFormWithScannedImages();
+});
+
+function submitFormWithScannedImages() {
+  if (
+    scanner.submitFormWithImages('form-save-db', imagesScanned, function (xhr) {
+      if (xhr.readyState == 4) {
+        console.log(xhr);
+        // 4: request finished and response is ready
+        document.getElementById('server_response').innerHTML =
+          '<h2>Response from the server: </h2>' + xhr.responseText;
+        document.getElementById('images').innerHTML = ''; // clear images
+        imagesScanned = [];
+      }
+    })
+  ) {
+    document.getElementById('server_response').innerHTML =
+      'Submitting, please stand by ...';
+  } else {
+    document.getElementById('server_response').innerHTML =
+      'Form submission cancelled. Please scan first.';
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'storage/' });
 const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-res.sendFile(path.join(__dirname,'../views/index.html'))
-// res.render('index.ejs')
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+
+const cpUpload = upload.fields([
+  { name: 'avatar', maxCount: 1 },
+  { name: 'gallery', maxCount: 8 },
+]);
+router.post('/form-save-db', cpUpload, function (req, res) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+  console.log(req.files);
+});
+
 
 module.exports = router;
